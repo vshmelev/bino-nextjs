b08ffd23216b3e66dbc48c6ecc378cf523d764ba
vshmelev <vshmelev@gmail.com>
2021-10-19 09:34:47 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a> and <a href="http://bino.monster">Bino Dev Site</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a> and <a href="http://bino.monster">Bino Dev Site!</a>
         </h1>
 
         <p className={styles.description}>
